0136ed64b971f8437978986175508030836f67d5
Brian Horvat <brianghorvat@gmail.com>
2018-12-31 03:03:32 -0500

    Add simple API wrapper for Prismic

---
+++ src/api/prismic.js
@@ -0,0 +1,10 @@
+import prismic from 'prismic-javascript'
+
+export default {
+    endpointUrl: 'https://benhorvat.prismic.io/api/v2',
+
+    fetch(predicate, endpoint = this.endpointUrl, apiOptions = {}) {
+        return prismic.getApi(endpoint)
+        .then(api => api.query(predicate, apiOptions))
+    }
+}
