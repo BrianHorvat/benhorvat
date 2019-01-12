2b34fda6735d84358f87100152dab72e23c6a013
Brian Horvat <brianghorvat@gmail.com>
2019-01-12 01:52:58 -0500

    Remove old resource plugin and associated files

--- src/resources/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Gallery } from './Gallery'
-export { default as GalleryFeed } from './GalleryFeed'
-export { default as GalleryPost } from './GalleryPost'
-export { default as GalleryAbout } from './GalleryAbout'
-export { default as GalleryTestimonials } from './GalleryTestimonials'
