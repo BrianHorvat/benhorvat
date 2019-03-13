d10670cd60c49cd735c8497e8ae173281d5f63f6
Brian Horvat <brianghorvat@gmail.com>
2019-03-13 04:31:25 -0400

    Further work on contact form

--- src/pages/GalleryContact.vue
+++ src/pages/GalleryContact.vue
@@ -6,7 +6,7 @@
           <h1>Contact</h1>
           <div v-html="contact.blurb"></div>
           <form
-            class="contact-form"
+            class="form form-grid-2"
             method="POST"
             :action="'https://formspree.io/' + meta.social.email"
           >
@@ -59,12 +59,42 @@
               <p
                 class="field-desc"
               >To help make scheduling your session as easy as possible, please give a range of possible dates.</p>
-              <div class="field-item">
-                <input name="date" type="date" :min="Date.now()" required>
+              <div v-for="(range, index) in ranges" :key="range.id" class="contact-date-row">
+                <div class="field-item">
+                  <input
+                    type="date"
+                    :min="(new Date()).toISOString().split('T')[0]"
+                    :value="range.start && range.start.toISOString().split('T')[0]"
+                    @input="range.start = $event.target.valueAsDate"
+                    required
+                  >
+                </div>
+                <div class="field-item">
+                  <input
+                    type="date"
+                    :min="(new Date()).toISOString().split('T')[0]"
+                    :value="range.end && range.end.toISOString().split('T')[0]"
+                    @input="range.end = $event.target.valueAsDate"
+                    required
+                  >
+                </div>
+                <input
+                  type="text"
+                  v-if="range.start && range.end"
+                  :value="range.start.toDateString() + ' to ' + range.end.toDateString()"
+                >
+                <button type="button" @click="ranges.splice(index, 1)" v-if="ranges.length > 1">
+                  <icon-base icon-name="Remove Date">
+                    <icon-trash/>
+                  </icon-base>
+                </button>
               </div>
-              <div class="field-item">
-                <input name="date" type="date" :min="Date.now()" required>
-              </div>
+              <button type="button" @click="addNewRange()">
+                <icon-base icon-name="Add Date">
+                  <icon-plus/>
+                </icon-base>
+                <span>Add Another Date</span>
+              </button>
             </div>
 
             <div class="field full-width">
@@ -146,11 +176,13 @@ import IconFacebook from '../components/icons/IconFacebook'
 import IconEmail from '../components/icons/IconEmail'
 import IconInstagram from '../components/icons/IconInstagram'
 import IconPhone from '../components/icons/IconPhone'
+import IconPlus from '../components/icons/IconPlus'
+import IconTrash from '../components/icons/IconTrash'
 import StyledRadio from '../components/StyledRadio'
 
 export default {
   components: {
-    IconBase, IconFacebook, IconEmail, IconInstagram, IconPhone, StyledRadio
+    IconBase, IconFacebook, IconEmail, IconInstagram, IconPhone, IconPlus, IconTrash, StyledRadio
   },
 
   name: 'gallery-contact',
@@ -160,7 +192,14 @@ export default {
       session: {
         type: '',
         location: ''
-      }
+      },
+      ranges: [
+        {
+          id: 0,
+          start: null,
+          end: null
+        }
+      ]
     }
   },
 
@@ -169,6 +208,16 @@ export default {
       meta: state => state.gallery.meta,
       contact: state => state.contact.contact
     }),
+  },
+
+  methods: {
+    addNewRange() {
+      this.ranges.push({
+        id: this.ranges.length,
+        start: null,
+        end: null
+      })
+    }
   },
 
   created() {
