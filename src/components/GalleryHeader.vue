48f9535b879bf345fd82e3fa77d4acab917af1ba
Brian Horvat <brianghorvat@gmail.com>
2019-03-22 12:48:19 -0400

    Add swipable link bar to mobile header, use icon instead of svg

--- src/components/GalleryHeader.vue
+++ src/components/GalleryHeader.vue
@@ -3,28 +3,20 @@
     <nav class="nav">
       <transition name="grow">
         <a v-if="this.$route.name !== 'home'" id="back" @click="navBack" tabindex="0">
-          <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"></path>
-          </svg>
+          <icon-base name="Go Back">
+            <icon-back/>
+          </icon-base>
         </a>
       </transition>
-      <mq-layout :mq="['mobile', 'tablet']" class="nav-bar mobile">
-        <div class="nav-line"></div>
-        <div class="nav-item site-title">
-          <router-link to="/" @click.native="menuClose">
-            <span>Benjamin Horvat</span>
-            <span>Photography</span>
-          </router-link>
-        </div>
-        <div class="nav-line"></div>
-      </mq-layout>
+
       <mq-layout mq="desktop+" class="nav-bar desktop">
         <div class="nav-line"></div>
         <div class="nav-menu left" :class="menuClass">
-          <div v-for="sort in sorts" :key="sort.uid" class="nav-item" >
-            <router-link :to="{name: 'category', params: { category: sort.uid } }" @click.native="menuClose">
-              {{ sort.title }}
-            </router-link>
+          <div v-for="sort in sorts" :key="sort.uid" class="nav-item">
+            <router-link
+              :to="{name: 'category', params: { category: sort.uid } }"
+              @click.native="menuClose"
+            >{{ sort.title }}</router-link>
           </div>
         </div>
         <div class="nav-item site-title">
@@ -35,13 +27,12 @@
         </div>
         <div class="nav-menu right" :class="menuClass">
           <div class="nav-item has-dropdown">
-            <span>Info</span>
+            <span>Information</span>
             <div class="nav-dropdown">
               <router-link :to="{name: 'about'}" @click.native="menuClose">About Me</router-link>
               <router-link :to="{name: 'process'}" @click.native="menuClose">The Process</router-link>
               <router-link :to="{name: 'testimonials'}" @click.native="menuClose">Testimonials</router-link>
             </div>
-            
           </div>
           <div class="nav-item">
             <router-link :to="{name: 'contact'}" @click.native="menuClose">Contact</router-link>
@@ -49,46 +40,79 @@
         </div>
         <div class="nav-line"></div>
       </mq-layout>
+
+      <mq-layout :mq="['mobile', 'tablet']" class="nav-bar mobile">
+        <div class="nav-item site-title">
+          <router-link to="/" @click.native="menuClose">
+            <span>Benjamin Horvat</span>
+            <span>Photography</span>
+          </router-link>
+        </div>
+        <div class="nav-menu">
+          <div v-for="sort in sorts" :key="sort.uid" class="nav-item">
+            <router-link
+              :to="{name: 'category', params: { category: sort.uid } }"
+              @click.native="menuClose"
+            >{{ sort.title }}</router-link>
+          </div>
+          <div class="nav-item">
+            <router-link :to="{name: 'about'}" @click.native="menuClose">About Me</router-link>
+          </div>
+          <div class="nav-item">
+            <router-link :to="{name: 'process'}" @click.native="menuClose">The Process</router-link>
+          </div>
+          <div class="nav-item">
+            <router-link :to="{name: 'testimonials'}" @click.native="menuClose">Testimonials</router-link>
+          </div>
+          <div class="nav-item">
+            <router-link :to="{name: 'contact'}" @click.native="menuClose">Contact</router-link>
+          </div>
+        </div>
+      </mq-layout>
     </nav>
   </header>
 </template>
 
 <script>
-  import { mapState } from 'vuex'
+import { mapState } from 'vuex'
+import IconBase from '../components/icons/IconBase'
+import IconBack from '../components/icons/IconBack'
 
-  export default {
-    name: 'gallery-header',
+export default {
+  name: 'gallery-header',
 
-    data () {
+  components: { IconBase, IconBack },
+
+  data() {
+    return {
+      menuActive: false
+    }
+  },
+
+  computed: {
+    ...mapState({
+      meta: state => state.gallery.meta,
+      sorts: state => state.gallery.sorts
+    }),
+    menuClass() {
       return {
-        menuActive: false
-      }
-    },
-
-    computed: {
-      ...mapState({
-        meta: state => state.gallery.meta,
-        sorts: state => state.gallery.sorts
-      }),
-      menuClass () {
-        return {
-          'menu-active': this.menuActive
-        }
-      }
-    },
-
-    methods: {
-      navBack () {
-        this.$router.go(-1)
-        this.$emit('back')
-        this.menuClose()
-      },
-      menuClose () {
-        this.menuActive = false
-      },
-      menuToggle () {
-        this.menuActive = !this.menuActive
+        'menu-active': this.menuActive
       }
     }
+  },
+
+  methods: {
+    navBack() {
+      this.$router.go(-1)
+      this.$emit('back')
+      this.menuClose()
+    },
+    menuClose() {
+      this.menuActive = false
+    },
+    menuToggle() {
+      this.menuActive = !this.menuActive
+    }
   }
+}
 </script>
