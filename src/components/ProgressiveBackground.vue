f7b238b1d0cd0c638342439c22c9e946b094dfed
Brian Horvat <brianghorvat@gmail.com>
2019-04-22 23:44:34 -0400

    Export shared code in progressive images to mixin

--- src/components/ProgressiveBackground.vue
+++ src/components/ProgressiveBackground.vue
@@ -1,59 +1,30 @@
 <template>
   <div class="progressive-background" :class="imageClass">
     <transition name="progressive-image-fade">
-      <img v-if="loaded" :src="src" ref="main">
+      <img v-show="loaded" :src="src" ref="main">
     </transition>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'progressive-background',
-    props: {
-      src: {
-        type: String,
-        required: true
-      }
-    },
-    data: function () {
+import ProgressiveImageLoader from './mixins/ProgressiveImageLoader.js'
+
+export default {
+  name: 'progressive-background',
+
+  mixins: [ProgressiveImageLoader],
+
+  computed: {
+    imageClass() {
       return {
-        loaded: false,
-        image: null
+        loading: !this.loaded
       }
     },
-    computed: {
-      imageClass () {
-        return {
-          loading: !this.loaded
-        }
-      },
-      imageStyle () {
-        return {
-          backgroundImage: this.image ? 'url(' + this.image + ')' : ''
-        }
-      }
-    },
-    watch: {
-      src () {
-        this.loadImage()
-      }
-    },
-    mounted () {
-      this.loadImage()
-    },
-    methods: {
-      loadImage: function () {
-        const image = new Image()
-        this.image = null
-
-        image.onload = () => {
-          this.image = this.src
-          this.loaded = true
-          this.$emit('onLoad', image.src)
-        }
-
-        image.src = this.src
+    imageStyle() {
+      return {
+        backgroundImage: this.image ? 'url(' + this.image + ')' : ''
       }
     }
-  }
+  },
+}
 </script>
--- src/components/ProgressiveImage.vue
+++ src/components/ProgressiveImage.vue
@@ -1,51 +1,28 @@
 <template>
   <div class="progressive-image">
     <transition name="progressive-image-fade">
-      <img v-if="loaded" :src="src" ref="main">
+      <img v-show="loaded" :src="src" ref="main">
     </transition>
     <svg v-if="!loaded" class="spinner" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
-      <circle class="path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>
+      <circle
+        class="path"
+        fill="none"
+        stroke-width="6"
+        stroke-linecap="round"
+        cx="33"
+        cy="33"
+        r="30"
+      ></circle>
     </svg>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'progressive-image',
-    props: {
-      src: {
-        type: String,
-        required: true
-      }
-    },
-    data: function () {
-      return {
-        loaded: false,
-        image: null
-      }
-    },
-    computed: {},
-    watch: {
-      src () {
-        this.loadImage()
-      }
-    },
-    mounted () {
-      this.loadImage()
-    },
-    methods: {
-      loadImage: function () {
-        const image = new Image()
-        this.image = null
+import ProgressiveImageLoader from './mixins/ProgressiveImageLoader.js'
 
-        image.onload = () => {
-          this.image = this.src
-          this.loaded = true
-          this.$emit('onLoad', image.src)
-        }
+export default {
+  name: 'progressive-image',
 
-        image.src = this.src
-      }
-    }
-  }
+  mixins: [ProgressiveImageLoader],
+}
 </script>
